b4348557384cc9bc2f37a5f074cbe3c71e1120f3
Gelo <angelo.delossantos000@gmail.com>
2024-06-23 11:45:40 +0800

    New DB Initial [June 21]

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Address;
 class AddressSeeder extends Seeder
 {
     /**
